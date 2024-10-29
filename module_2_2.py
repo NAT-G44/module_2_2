c3d793f55002900d4f95d7a7ea4aec01b33a7d37
NAT-G44 <gr.natasha.2003@gmail.com>
2024-10-29 21:48:45 +0800

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,23 @@
+first_number=51
+print(first_number)
+second_number=25
+print(second_number)
+third_number=50
+print(third_number)
+if first_number == second_number == third_number:
+    print(3)
+elif first_number != second_number != third_number:
+    print(0)
+first_number=51
+print(first_number)
+second_number=25
+print(second_number)
+third_number=51
+print(third_number)
+if first_number == second_number or first_number == third_number:
+    print(2)
+
+
+
+
+
